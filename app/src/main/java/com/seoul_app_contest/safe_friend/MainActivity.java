94e82a382fcea833c84148a36150f2e27c8616d9
sgk0713 <sgk0713@gmail.com>
2018-09-10 16:16:39 +0900

    Initial commit

---
+++ app/src/main/java/com/seoul_app_contest/safe_friend/MainActivity.java
@@ -0,0 +1,13 @@
+package com.seoul_app_contest.safe_friend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
