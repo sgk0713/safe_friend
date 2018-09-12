953d0e26c06c40e7440dbac8f565aa2bd309799e
ParkBeomMin <club20608@gmail.com>
2018-09-13 00:50:33 +0900

    메인화면 레이아웃 파이어베이스 인증 연동

---
+++ app/src/main/java/com/seoul_app_contest/safe_friend/Register/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.seoul_app_contest.safe_friend.Register;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.seoul_app_contest.safe_friend.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
